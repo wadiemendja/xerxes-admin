619cc0a59254cd8573a4daab9827e01edcb0c118
Wadie Mendja <mendja2014@gmail.com>
2023-01-31 13:19:52 +0100

    commit

--- public/render.js
+++ public/render.js
@@ -1,6 +1,6 @@
 function renderHeader() {
     return `
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg" style="background-color: #e3f2fd;">
     <div class="container-fluid">
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -8,7 +8,7 @@ function renderHeader() {
       <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Accueil [demondes]</a>
+            <a class="nav-link active" aria-current="page" href="/">Accueil [demandes]</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/add-request">Ajouter un demande</a>
@@ -17,7 +17,7 @@ function renderHeader() {
             <a class="nav-link">A propos du logiciel</a>
           </li>
         </ul>
-        <form class="d-flex" role="search">
+        <form class="d-flex " role="search">
           <input class="form-control me-2" id="searchInput" type="search" placeholder="Rechercher par nom, prenon, telephone ou wilaya.." aria-label="Search" style="width: 400px">
           <button class="btn btn-outline-success" type="submit" id="searchBtn">Rechercher</button>
         </form>
